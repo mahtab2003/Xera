6d9a5085d48b1b242a12317bb21d29c757d8dbd9
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-05-22 15:28:16 +0700

    Fixed account settings

--- template/page/user/account_settings.php
+++ template/page/user/account_settings.php
@@ -43,7 +43,7 @@
 			<div class="card-title">Deactivation</div>
 		</div>
 		<div class="card-body">
-			<?= form_open('u/account_settings') ?>
+			<?= form_open('u/account_settings/'.$id) ?>
 				<div class="row">
 					<div class="col-sm-12">
 						<div class="alert alert-danger">
