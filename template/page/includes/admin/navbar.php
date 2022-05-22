d96116aaec5a6dce4183eaf8da6a332124345f1f
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-05-22 15:30:06 +0700

    Updated navbar

--- template/page/includes/admin/navbar.php
+++ template/page/includes/admin/navbar.php
@@ -4,7 +4,7 @@
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-			<a href=".">
+			<a href="<?= base_url()?>">
 				<img src="<?= base_url()?>assets/img/logo.png" width="110" height="32" alt="Logo" class="navbar-brand-image">
 			</a>
 		</h1>
