be7ebe45fbf4feb8c778bd714c1332bd27ad9dab
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-06-18 14:37:32 +0500

    Add files via upload

---
+++ template/page/user/create_ssl.php
@@ -0,0 +1,28 @@
+<div class="container-xl">
+	<div class="page-header d-print-none">
+		<h2 class="page-title py-3">
+			Create SSL
+		</h2>
+	</div>
+	<div class="card p-2 mb-3">
+		<div class="card-body">
+			<?= form_open('u/create_ssl') ?>
+				<div class="row">
+					<div class="col-sm-12 mb-2">
+						<label class="form-label">CSR Code</label>
+						<textarea class="form-control" name="csr" placeholder="CSR code here...." style="min-height: 250px;"></textarea>
+					</div>
+					<?php if($this->grc->is_active()):?>
+						<div class="col-sm-12 mb-2">
+							<div class="g-recaptcha" data-sitekey="<?= $this->grc->get_site_key();?>"></div>
+							<script src='https://www.google.com/recaptcha/api.js' async defer ></script>
+						</div>
+					<?php endif ?>
+					<div class="col-sm-12">
+						<input type="submit" name="create" value="Request" class="btn btn-primary btn-pill">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
