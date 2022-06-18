9139036b214760cff4822542fe4683fbb3a27d22
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-06-18 14:36:59 +0500

    Add files via upload

---
+++ template/page/admin/ssl.php
@@ -0,0 +1,50 @@
+<div class="container-xl">
+	<div class="page-header d-print-none">
+		<div class="row align-items-center">
+			<div class="col">
+				<h2 class="page-title py-3">
+					SSL Certificates
+				</h2>
+			</div>
+		</div>
+	</div>
+	<div class="card mb-3 rounded">
+		<div class="card-header">
+			<div class="card-title">Your Certificates</div>
+		</div>
+		<div class="table-responsive">
+			<table class="table card-table table-transparent text-nowrap table-nowrap">
+				<thead>
+					<tr>
+						<th width="5%">ID</th>
+						<th width="75%">Domain</th>
+						<th width="10%">Method</th>
+						<th width="10%">Status</th>
+						<th width="10%" class="text-center">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php if (count($list) > 0): ?>
+						<?php foreach ($list as $item): ?>
+							<tr>
+								<td><?php echo $count = $count ?? 1 ?></td>
+								<td><?= $item['domain'] ?></td>
+								<td>DNS</td>
+								<td><?= strtoupper($item['status']) ?></td>
+								<td><a href="<?= base_url().'a/view_ssl/'.$item['key'] ?>" class="btn btn-sm">Manage</a></td>
+							</tr>
+							<?php $count += 1; ?>
+						<?php endforeach; ?>
+					<?php else: ?>
+						<tr>
+							<td colspan="5" class="text-center">Nothing to show</td>
+						</tr>
+					<?php endif ?>
+				</tbody>
+			</table>
+		</div>
+		<div class="card-footer py-2">
+			<?= count($list) ?> SSL Certificates
+		</div>
+	</div>
+</div>
