220acaa14e4167d5ef8fc3ae13b00e7520241afa
Fury_Craft <44116544+FuryCraft@users.noreply.github.com>
2022-08-02 11:43:34 +0200

    Added Go premium button

--- template/page/includes/user/navbar.php
+++ template/page/includes/user/navbar.php
@@ -9,6 +9,16 @@
 			</a>
 		</h1>
 		<div class="navbar-nav flex-row order-md-last">
+			<div class="nav-item d-none d-md-flex me-3">
+				<div class="btn-list">
+					<a href="<?= base_url() ?>go/ifastnet" class="btn bg-orange" target="_blank">
+						<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 512 512" stroke-width="2" stroke="currentColor" fill="currentColor" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256c141.4 0 256-114.6 256-256S397.4 0 256 0zM382.8 246.1C380.3 252.1 374.5 256 368 256h-64v96c0 17.67-14.33 32-32 32h-32c-17.67 0-32-14.33-32-32V256h-64C137.5 256 131.7 252.1 129.2 246.1C126.7 240.1 128.1 233.3 132.7 228.7l112-112c6.248-6.248 16.38-6.248 22.62 0l112 112C383.9 233.3 385.3 240.1 382.8 246.1z" />
+						</svg>
+						Go premium
+					</a>
+				</div>
+			</div>
 			<div class="nav-item dropdown">
 				<a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
 					<span class="avatar avatar-sm" style="background-image: url(<?= $this->user->get_avatar() ?>);"></span>
