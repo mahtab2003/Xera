36d5b93952b6b6d7671c01e8f6903bc70e7465f2
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-08-24 13:57:30 +0500

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -453,7 +453,7 @@ $config['csrf_token_name'] = 'COOKIE_PREFIX_VALUEcsrf_token';
 $config['csrf_cookie_name'] = 'csrf_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array('c/mofh');
+$config['csrf_exclude_uris'] = array('c/mofh', 'c/github_oauth');
 
 /*
 |--------------------------------------------------------------------------
