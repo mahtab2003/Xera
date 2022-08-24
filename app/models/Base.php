77fccca14fa457ddbfcdea24d90fb5b35a39f347
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-08-24 16:46:12 +0500

    Add files via upload

--- app/models/Base.php
+++ app/models/Base.php
@@ -1,9 +1,12 @@
-<?php
+<?php 
 
-if (!file_exists(APPPATH . 'logs/install.json')) {
+if(!file_exists(APPPATH.'logs/install.json'))
+{
 	header('location: install.php');
-} elseif (file_exists(APPPATH . 'logs/install.json') and file_exists(APPPATH . '../install.php')) {
-	unlink(APPPATH . '../install.php');
+}
+elseif(file_exists(APPPATH.'logs/install.json') AND file_exists(APPPATH.'../install.php'))
+{
+	unlink(APPPATH.'../install.php');
 }
 
 class Base extends CI_Model
@@ -11,7 +14,8 @@ class Base extends CI_Model
 
 	function __construct()
 	{
-		if (!get_cookie('lang')) {
+		if(!get_cookie('lang'))
+		{
 			set_cookie('lang', 'english', 365 * 86400);
 		}
 	}
@@ -20,7 +24,8 @@ class Base extends CI_Model
 	{
 		$this->lang->load($filename, get_cookie('lang'));
 		$res = $this->lang->line($line);
-		if ($res !== false) {
+		if($res !== false)
+		{
 			return $res;
 		}
 		return '...';
@@ -29,7 +34,8 @@ class Base extends CI_Model
 	function get_hostname()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
+		if($res !== false)
+		{
 			return $res['base_name'];
 		}
 		return false;
@@ -38,7 +44,8 @@ class Base extends CI_Model
 	function set_hostname($name)
 	{
 		$res = $this->update_base('name', $name);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
@@ -47,8 +54,10 @@ class Base extends CI_Model
 	function get_template()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
-			if (is_dir(APPPATH . 'template/' . $res['base_template'])) {
+		if($res !== false)
+		{
+			if(is_dir(APPPATH.'../template/'.$res['base_template']))
+			{
 				return $res['base_template'];
 			}
 			return 'default';
@@ -59,16 +68,18 @@ class Base extends CI_Model
 	function set_template($template)
 	{
 		$res = $this->update_base('template', $template);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
 	}
-
+	
 	function get_email()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
+		if($res !== false)
+		{
 			return $res['base_email'];
 		}
 		return false;
@@ -77,16 +88,18 @@ class Base extends CI_Model
 	function set_email($email)
 	{
 		$res = $this->update_base('email', $email);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
 	}
-
+	
 	function get_fourm()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
+		if($res !== false)
+		{
 			return $res['base_fourm'];
 		}
 		return false;
@@ -95,28 +108,32 @@ class Base extends CI_Model
 	function set_fourm($fourm)
 	{
 		$res = $this->update_base('fourm', $fourm);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
 	}
-
+	
 	function is_active()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
-			if ($res['base_status'] === 'active') {
+		if($res !== false)
+		{
+			if($res['base_status'] === 'active')
+			{
 				return true;
 			}
 			return false;
 		}
 		return false;
 	}
-
+	
 	function get_status()
 	{
 		$res = $this->fetch_base();
-		if ($res !== false) {
+		if($res !== false)
+		{
 			return $res['base_status'];
 		}
 		return false;
@@ -124,13 +141,17 @@ class Base extends CI_Model
 
 	function set_status(bool $status)
 	{
-		if ($status === true) {
+		if($status === true)
+		{ 
 			$status = 'active';
-		} else {
+		}
+		else
+		{
 			$status = 'inactive';
 		}
 		$res = $this->update_base('status', $status);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
@@ -139,7 +160,8 @@ class Base extends CI_Model
 	private function fetch_base()
 	{
 		$res = $this->fetch('is_base', ['id' => 'xera_base']);
-		if (count($res) > 0) {
+		if(count($res)>0)
+		{
 			return $res[0];
 		}
 		return false;
@@ -151,7 +173,8 @@ class Base extends CI_Model
 			[$field => $value],
 			['id' => 'xera_base']
 		);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
@@ -164,7 +187,8 @@ class Base extends CI_Model
 		$this->db->where($where);
 		$this->db->set($data);
 		$res = $this->db->update($table);
-		if ($res) {
+		if($res)
+		{
 			return true;
 		}
 		return false;
@@ -172,7 +196,8 @@ class Base extends CI_Model
 
 	function fetch($table, $where = [], $prefix = 'base_', $data = '*')
 	{
-		if (count($where) > 0) {
+		if(count($where)>0)
+		{
 			$where = remap_array($prefix, $where);
 		}
 		$this->db->where($where);
@@ -182,3 +207,5 @@ class Base extends CI_Model
 		return $res;
 	}
 }
+
+?>
