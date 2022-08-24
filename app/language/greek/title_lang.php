f28944a1e7d0490d69f392b0c574d9d3fe000a2d
Bop V2 <97548973+bopv2@users.noreply.github.com>
2022-08-24 03:25:13 -0700

    Added greek language (Ελλινικά)
    Probably nobody using xera is from greece so its useless
    but still it is something :D

---
+++ app/language/greek/title_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['login'] = 'Σύνδεση';
+$lang['register'] = 'Εγγραφή';
+$lang['forget_password'] = 'Ξεχάσα τον κωδικό πρόσβασης';
+$lang['reset_password'] = 'Αλλαγη κωδικου';
+$lang['dashboard'] = 'Dashboard';
+$lang['settings'] = 'Ρυθμίσεις';
+$lang['api_settings'] = 'Ρυθμίσεις API';
+$lang['accounts'] = 'Λογαριασμοί';
+$lang['create_account'] = 'Δημιουργήστε λογαριασμό';
+$lang['view_account'] = 'Προβολή λογαριασμού';
+$lang['ssl'] = 'Πιστοποιητικά SSL';
+$lang['create_ssl'] = 'Ζητήστε πιστοποιητικό SSL';
+$lang['view_ssl'] = 'Προβολή πιστοποιητικού SSL';
+$lang['account_settings'] = 'Ρυθμίσεις λογαριασμού';
+$lang['tickets'] = 'Εισιτήρια υποστήριξης';
+$lang['create_ticket'] = 'Δημιουργία εισιτηρίου';
+$lang['view_ticket'] = 'Προβολή εισιτηρίου';
+$lang['domain_checker'] = 'Έλεγχος τομέα';
+$lang['cpanel_login'] = 'Είσοδος cPanel';
+$lang['user_inactive'] = 'Χρήστης Ανενεργός';
+$lang['err_404'] = 'Το αρχείο δε βρέθηκε';
+$lang['err_500'] = 'Μη διαθέσιμη υπηρεσία';
+$lang['err_503'] = 'Απαγορευμένη πρόσβαση';
+$lang['upgrade_hosting'] = 'Αναβάθμιση';
+
+?>
