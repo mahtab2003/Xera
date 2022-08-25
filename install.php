ed29ca1d77f63737214fb212d283f992daf498b0
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-08-25 08:01:11 +0500

    Update install.php

--- install.php
+++ install.php
@@ -218,7 +218,7 @@ elseif(isset($_GET['step']) AND $_GET['step'] == 2 AND isset($_POST['submit']))
 
 		$sql = mysqli_query($mysqli, "DROP TABLE IF EXISTS `is_user`;");
 
-		$sql = mysqli_query($mysqli, "CREATE TABLE `is_user` (`user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,`user_name` varchar(50) NOT NULL,`user_email` varchar(100) NOT NULL,`user_password` varchar(100) NOT NULL,`user_key` varchar(16) NOT NULL,`user_rec` varchar(32) NOT NULL,`user_status` varchar(8) NOT NULL,`user_date` varchar(20) NOT NULL
+		$sql = mysqli_query($mysqli, "CREATE TABLE `is_user` (`user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,`user_name` varchar(50) NOT NULL,`user_email` varchar(100) NOT NULL,`user_password` varchar(100) NOT NULL,`user_key` varchar(16) NOT NULL,`user_rec` varchar(32) NOT NULL,`user_status` varchar(8) NOT NULL,`user_date` varchar(20) NOT NULL, `user_oauth` varchar(10) NOT NULL DEFAULT 'disabled'
 		);");
 
 		$sql = mysqli_query($mysqli, "DROP TABLE IF EXISTS `is_admin`;");
