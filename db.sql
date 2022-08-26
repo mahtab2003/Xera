29fe1cf56e1154c0870baf8a26508b43ee84b5bd
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-08-26 12:22:54 +0500

    Add files via upload

--- db.sql
+++ db.sql
@@ -9,6 +9,7 @@ CREATE TABLE `is_base` (
 	`base_fourm` varchar(100) NOT NULL,
 	`base_template` varchar(100) NOT NULL DEFAULT 'default',
 	`base_status` varchar(8) NOT NULL
+	`base_rpp` int(10) NOT NULL DEFAULT '1',
 );
 
 -- Insert default record for `is_base`
