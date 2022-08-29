1976338d34dc5721181ff75c1036cd1aa36b98ec
WavaAdrian <87927419+WavaAdrian@users.noreply.github.com>
2022-08-29 14:21:40 +0200

    Create success_lang.php

---
+++ app/language/norwegian/success_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['login_msg'] = 'Logget på vellykket.';
+$lang['register_msg'] = 'Brukeren er registrert.';
+$lang['forget_msg'] = 'Passord tilbakestilt. Sjekk innboksen din.';
+$lang['reset_msg'] = 'Passord tilbakestilt.';
+$lang['logout_msg'] = 'Logget ut.';
+$lang['theme_msg'] = 'Grensesnittinnstillingene ble endret..';
+$lang['name_msg'] = 'Brukernavnet ble oppdatert.';
+$lang['user_pass_msg'] = 'Brukerpassordet ble oppdatert.';
+$lang['ticket_msg'] = 'Billetten ble opprettet.';
+$lang['ticket_closed_msg'] = 'Billetten ble lukket.';
+$lang['ticket_opened_msg'] = 'Billetten ble åpnet.';
+$lang['reply_msg'] = 'Billettsvaret ble lagt til.';
+$lang['domain_selected_msg'] = 'Domenenavnet er valgt.';
+$lang['label_updated_msg'] = 'Kontoetiketten er oppdatert.';
+$lang['ssl_created_msg'] = 'SSL-sertifikat ble forespurt.';
+$lang['ssl_deleted_msg'] = 'SSL-sertifikatet ble slettet.';
+$lang['account_msg'] = 'Konto opprettet.';
+$lang['ssl_cancelled_msg'] = 'SSL-sertifikatet ble slettet.';
