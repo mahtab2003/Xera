9122005ffdc9c106cb51aaf667c81006ca2ba008
WavaAdrian <87927419+WavaAdrian@users.noreply.github.com>
2022-08-29 13:55:50 +0200

    Create button_lang.php

---
+++ app/language/norwegian/button_lang.php
@@ -0,0 +1,39 @@
+<?php 
+
+$lang['signin'] = 'Logg inn';
+$lang['register'] = 'Registrere';
+$lang['login'] = 'Logg Inn';
+$lang['change'] = 'Endring';
+$lang['send_me_link'] = 'Send meg nytt passord';
+$lang['reset_password'] = 'Tilbakestille passord';
+$lang['create'] = 'Skape';
+$lang['manage'] = 'Manage';
+$lang['deactivate'] = 'Deaktiver';
+$lang['redirect'] = 'Omdiriger nå';
+$lang['check_domain'] = 'Sjekk domene';
+$lang['configure'] = 'Konfigurer';
+$lang['subdomain'] = 'Underdomene';
+$lang['custom_domain'] = 'Egendefinert domene';
+$lang['check_availibilty'] = 'Sjekk tilgjengelighet';
+$lang['create_account'] = 'Opprett konto';
+$lang['view_accounts'] = 'Se kontoer';
+$lang['request'] = 'Be om';
+$lang['search'] = 'Søk';
+$lang['resend_email'] = 'Send e-post på nytt';
+$lang['logout'] = 'Logg ut';
+$lang['control_panel'] = 'Kontrollpanel';
+$lang['file_manager'] = 'Filbehandler';
+$lang['reactivate'] = 'Aktiver på nytt';
+$lang['settings'] = 'Innstillinger';
+$lang['open_ticket'] = 'Åpne billett';
+$lang['delete'] = 'Slett';
+$lang['cancel'] = 'Avbryt';
+$lang['here'] = 'her';
+$lang['done'] = 'Ferdig';
+$lang['close_ticket'] = 'Lukk billett';
+$lang['add_reply'] = 'Legg til svar';
+$lang['redirect_now'] = 'Omdiriger nå';
+$lang['github_signin'] = 'Logg på med Github';
+
+?>
+Norwegian (Bokmål)
