4a2b1e3e23fd437a754e213991d51c87414c356b
WavaAdrian <87927419+WavaAdrian@users.noreply.github.com>
2022-08-29 14:13:23 +0200

    Create label_lang.php

---
+++ app/language/norwegian/label_lang.php
@@ -0,0 +1,31 @@
+<?php 
+
+$lang['email_address'] = 'Epostadresse';
+$lang['username'] = 'Brukernavn';
+$lang['password'] = 'Passord';
+$lang['old_password'] = 'Gammelt Passord';
+$lang['new_password'] = 'Nytt Passord';
+$lang['confirm_password'] = 'Bekreft Passord';
+$lang['your_name'] = 'Navnet ditt';
+$lang['recaptcha'] = 'Recaptcha';
+$lang['language'] = 'Språk';
+$lang['subject'] = 'Emne';
+$lang['content'] = 'Innhold';
+$lang['domain'] = 'Domene';
+$lang['theme'] = 'Tema';
+$lang['label'] = 'label';
+$lang['csr_code'] = 'CSR-kode';
+$lang['crt_code'] = 'CRT-kode';
+$lang['ca_code'] = 'CA-kode';
+$lang['reason'] = 'Årsaken';
+$lang['domain_name'] = 'Domenenavn';
+$lang['account_label'] = 'Kontoetikett';
+$lang['ok'] = 'OK';
+$lang['error'] = 'Feil';
+$lang['redirecting'] = 'Omdirigerer';
+$lang['or'] = 'eller';
+$lang['show_hide'] = 'Vis/Skjul';
+$lang['record_name'] = 'Opptaksnavn';
+$lang['record_content'] = 'Ta opp innhold';
+
+?>
