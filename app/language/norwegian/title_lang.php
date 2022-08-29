29fb3754a766e5faec464896f51f499249dcb22b
WavaAdrian <87927419+WavaAdrian@users.noreply.github.com>
2022-08-29 14:27:59 +0200

    Create title_lang.php

---
+++ app/language/norwegian/title_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['login'] = 'Logg på';
+$lang['register'] = 'Registrer deg';
+$lang['forget_password'] = 'Glem passord';
+$lang['reset_password'] = 'Tilbakestill passord';
+$lang['dashboard'] = 'Dashboard';
+$lang['settings'] = 'Innstillinger';
+$lang['api_settings'] = 'Api-innstillinger';
+$lang['accounts'] = 'Hosting-kontoer';
+$lang['create_account'] = 'Opprett konto';
+$lang['view_account'] = 'Se konto';
+$lang['ssl'] = 'SSL-sertifikater';
+$lang['create_ssl'] = 'Be om SSL-sertifikat';
+$lang['view_ssl'] = 'Se SSL-sertifikat';
+$lang['account_settings'] = 'Kontoinnstillinger';
+$lang['tickets'] = 'Støttebilletter';
+$lang['create_ticket'] = 'Opprett billett';
+$lang['view_ticket'] = 'Se billett';
+$lang['domain_checker'] = 'Domenekontroller';
+$lang['cpanel_login'] = 'cPanel-pålogging';
+$lang['user_inactive'] = 'Bruker inaktiv';
+$lang['err_404'] = 'Filen ikke funnet';
+$lang['err_500'] = 'Tjeneste utilgjengelig';
+$lang['err_503'] = 'Forbudt tilgang';
+$lang['upgrade_hosting'] = 'Oppgrader hosting';
+
+?>
