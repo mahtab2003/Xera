b9b3457260c61080c2f4bc40e85a3fec6bb87ec2
WavaAdrian <87927419+WavaAdrian@users.noreply.github.com>
2022-08-29 14:18:15 +0200

    Create paragraph_lang.php

---
+++ app/language/norwegian/paragraph_lang.php
@@ -0,0 +1,27 @@
+<?php 
+
+$lang['forget_password'] = 'Skriv inn e-postadressen din og passordet ditt vil bli tilbakestilt og sendt til deg på e-post.';
+$lang['account_limit'] = 'Du er i ferd med å nå grensen på 3 gratis hostingkontoer per bruker. Vennligst oppgrader til';
+$lang['better_service'] = 'for bedre service.';
+$lang['nothing_to_show'] = 'Ingenting å vise.';
+$lang['account_warning'] = 'Fjern alle domener og underdomener før du deaktiverer kontoen din, ellers vil alle domener og underdomener av denne kontoen bli låst for alltid.';
+$lang['cpanel_login'] = 'Vi skal nå omdirigere deg til cPanel.';
+$lang['account_created'] = 'Alt er satt opp riktig.';
+$lang['search_note'] = 'Søkeresultatet vil vises her når et domenenavn er søkt.';
+$lang['search_error'] = 'Unnskyld! dette domenenavnet er ikke knyttet til noen av våre webhotellkontoer.';
+$lang['oops_note'] = 'Oops... Du fant nettopp en feilside';
+$lang['user_inactive_note'] = 'Vi beklager, men kontoen din er inaktiv, vennligst sjekk innboksen din.';
+$lang['err_404_note'] = 'Vi beklager, men siden du leter etter ble ikke funnet.';
+$lang['err_500_note'] = 'Vi beklager, men tjenesten er ikke tilgjengelig akkurat nå.';
+$lang['err_503_note'] = 'Vi beklager, men siden du leter etter er forbudt.';
+$lang['account_pending'] = 'Denne vertskontoen venter og vil snart bli aktivert.';
+$lang['account_reactivating'] = 'Denne vertskontoen reaktiveres og vil snart bli aktivert.';
+$lang['account_deactivating'] = 'hostingkontoen hans deaktiveres og vil snart bli deaktivert.';
+$lang['account_suspended'] = 'Denne vertskontoen er suspendert av en eller annen grunn og vil bli fullstendig fjernet innen 30 dager.';
+$lang['account_deactivated'] = 'Denne hostingkontoen er deaktivert og vil bli fullstendig fjernet innen 30 dager.';
+$lang['account_note'] = 'Noen av vertsfunksjonene fungerer kanskje ikke. Det kan ta opptil 72 timer før vertskontoen fungerer som den skal.';
+$lang['no_reply_found'] = 'Finner ikke noe svar.';
+$lang['ticket_closed'] = 'Billetten var stengt. Klikk';
+$lang['to_reopen'] = 'å åpne igjen.';
+
+ ?>
